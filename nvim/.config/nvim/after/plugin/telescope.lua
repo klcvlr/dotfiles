e37ce0f318a4708b72addde027ebb8f02caf05fb
Kevin Lacavalerie <kevin.lacavalerie@gmail.com>
2023-11-05 22:19:44 +0100

    update nvim configuration

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -6,6 +6,9 @@ require('telescope').setup{
     }
 }
 
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+
 local builtin = require('telescope.builtin')
 
 -- Keymaps
