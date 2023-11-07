7c706aa9b419474559a8b9fb289fd7473bfbceed
Kevin Lacavalerie <kevin.lacavalerie@gmail.com>
2023-11-07 07:06:12 +0100

    update configuration

--- nvim/.config/nvim/lua/kevin/remap.lua
+++ nvim/.config/nvim/lua/kevin/remap.lua
@@ -9,6 +9,7 @@ vim.g.maplocalleader = ' '
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<Bs>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
